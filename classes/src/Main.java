d4b80e037a9df550d1abeed738f274d0ccc93ea3
Velihan <velihangozek@hotmail.com>
2022-06-04 19:13:26 +0300

    Add files via upload

---
+++ classes/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    CustomerManager customerManager; // = new CustomerManager();
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager = customerManager2;
+        customerManager.add();
+        customerManager.remove();
+        customerManager.update();
+
+        int number1 = 10;
+        int number2 = 20;
+        number2 = number1; // number2 = 10
+        number1 = 30;
+        System.out.println(number2);
+
+        int[] numbers = new int[] {1, 2, 3};
+        int[] numbers2 = new int[] {4, 5, 6};
+        numbers2 = numbers;
+        numbers[0] = 10;
+        System.out.println(numbers2[0]); // 10
+    }
+}
+
