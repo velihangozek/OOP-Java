a6cf80b9fbe4a2fc68558f2e93c4033ababe3532
Velihan Gözek <velihangozek@hotmail.com>
2022-06-11 21:16:20 +0300

    Add files via upload

---
+++ inheritanceDemo/src/KrediUI.java
@@ -0,0 +1,7 @@
+public class KrediUI {
+    // UI
+    //public void KrediHesapla(OgretmenKrediManager ogretmenKrediManager) {
+    public void krediHesapla(BaseKrediManager baseKrediManager) {
+        baseKrediManager.hesapla();
+    }
+}
---
+++ inheritanceDemo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+//	OgretmenKrediManager ogretmenKrediManager = new OgretmenKrediManager();
+//    ogretmenKrediManager.hesapla();
+        KrediUI krediUI = new KrediUI();
+        krediUI.krediHesapla(new OgretmenKrediManager());
+        krediUI.krediHesapla(new TarimKrediManager());
+        krediUI.krediHesapla(new AskerKrediManager());
+    }
+}
